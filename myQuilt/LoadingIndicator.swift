706a3280ec74003ab35eb87758d6a7928fee30ff
jakesheiner <jacob.sheiner@gmail.com>
2024-07-10 18:11:18 -0400

    added loading state

---
+++ myQuilt/LoadingIndicator.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingIndicator.swift
+//  myQuilt
+//
+//  Created by Jacob Sheiner on 7/10/24.
+//
+
+import SwiftUI
+
+struct LoadingIndicator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingIndicator()
+}
---
+++ myQuilt/LoadingThreeBalls.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingThreeBalls.swift
+//  myQuilt
+//
+//  Created by Jacob Sheiner on 7/10/24.
+//
+
+import SwiftUI
+
+struct LoadingThreeBalls: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingThreeBalls()
+}
